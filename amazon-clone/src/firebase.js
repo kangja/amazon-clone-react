9508dd15aff6ccaf0ee1229567c9a02be00583c7
hi <kangja1996@gmail.com>
2020-11-21 22:54:10 -0500

    progress

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC_r-mF_m9KvJOldKzOpsrbxJX6nWxLODE",
+  authDomain: "clone-93db3.firebaseapp.com",
+  databaseURL: "https://clone-93db3.firebaseio.com",
+  projectId: "clone-93db3",
+  storageBucket: "clone-93db3.appspot.com",
+  messagingSenderId: "621972127674",
+  appId: "1:621972127674:web:94cc4f03d53796695a2783"
+};
