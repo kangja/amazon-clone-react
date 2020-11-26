cd3a409e2733a4072b0efaf5c25e9eb1c90e872d
hi <kangja1996@gmail.com>
2020-11-26 00:29:55 -0500

    progress:

---
+++ amazon-clone/src/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Home.css"
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img
+            src=""
+      </div>
+    </div>
+  )
+}
+
+export default Home
