104f14f8fb6ed1202bda47b099c89805f025271e
hi <kangja1996@gmail.com>
2020-12-02 01:48:16 -0500

    progress

--- amazon-clone/src/Product.js
+++ amazon-clone/src/Product.js
@@ -2,8 +2,10 @@ import React from 'react'
 import "./Product.css"
 import { useStateValue } from './StateProvider';
 
-function Product( { id, title, image, price, rating }) {
-  const [ state  , dispatch] = useStateValue();
+function Product({ id, title, image, price, rating }) {
+  const [{ basket }, dispatch] = useStateValue();
+  
+  console.log("this is the basket >>> ", basket);
 
   const addToBasket = () => {
     //dispatch the item into the data layer
--- amazon-clone/src/reducer.js
+++ amazon-clone/src/reducer.js
@@ -1,16 +1,34 @@
 export const initialState = {
-  basekt: [],
+  basket: [],
 };
 
-const reducer = (state, action) => {
+// Selector
+export const getBasketTotal = (basket) =>
+  basket?.reduce((amount, item) => item.price + amount, 0); //tallies up the basket and sums up all the price and returns it. 
 
+const reducer = (state, action) => {
+  console.log(action);
   switch (action.type) {
-    case "ADD_TO_BASEKET":
+    case "ADD_TO_BASKET":
       return {
         ...state, //previous state of basket
         basket: [...state.basket, action.item] //previous state of basket + whatever we've decided add
       };
     
+    case "REMOVE_FROM_BASKET":
+      const index = state.basket.findIndex(
+        (basketItem) => basketItem.id === action.id
+      );
+      let newBasket = [...state.basket];
+
+      if (index >= 0) {
+
+      } else {
+        console.warn(
+          `Can't remove product (id: ${action.id}) as its not in basket!`
+        )
+      }
+      
     default:
       return state;
   }
