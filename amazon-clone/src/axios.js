afb78eb15969cdf71a3f57810167792232a27db0
hi <kangja1996@gmail.com>
2020-12-12 22:47:33 -0500

    finished

--- amazon-clone/src/axios.js
+++ amazon-clone/src/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5001/clone-93db3/us-central1/api" //THE API(cloud function) URL
-
+  baseURL: "https://us-central1-clone-93db3.cloudfunctions.net/api" //THE API(cloud function) URL
+  // "http://localhost:5001/clone-93db3/us-central1/api" 
 })
 
 export default instance;
